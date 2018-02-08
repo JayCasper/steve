a6bd612ca4bf17fc0ac4c73fd6290b4569df52c9
supermyle <kylemanley99@gmail.com>
2018-02-07 22:37:37 -0500

    Made changes

--- src/steve/Steve.java
+++ src/steve/Steve.java
@@ -1,27 +1,31 @@
 package steve;
+//Edited by Kyle Manley 02/07/2018
 import java.util.Scanner;
-
 
 public class Steve 
 {
-
-    
     public static void main(String[] args) 
     {
-            Scanner input = new Scanner (System.in);
-            System.out.println("enter a number");
-            int num = input.nextInt();
-            
-         
-            if(num % 2 == 0)
-            {
-                    System.out.println("your number," + num + " is even");
-            }
-            else
-            {
-                    System.out.println("your number," + num + " is odd");
-            }
-    }
-    
-        
+         Scanner input = new Scanner (System.in);
+         boolean even;
+         System.out.println("enter a number");
+         int num = input.nextInt();
+         if(num % 2 == 0)
+         {
+            even = true;
+         }
+         else
+         {
+            even = false;
+         }
+         System.out.print("Your number " + num + " is ");
+         if(even)
+         {
+            System.out.println("even");
+         }
+         if(!even)
+         {
+            System.out.println("odd");
+         }
+    }   
 }
